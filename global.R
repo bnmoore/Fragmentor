2e06302b516b2e75abae98b816b06745ccbcc5cd
bnmoore <benjamin.n.moore@gmail.com>
2021-11-13 23:38:17 -0800

    Faster update

---
+++ global.R
@@ -0,0 +1,20 @@
+library(stringr)
+library(tidyverse)
+library(OrgMassSpecR)
+
+ionTypeList = list("M", "M*", "a", "b", "b+H2O", "c", "x", "y", "z", "d", "w")
+
+#Atomic masses from https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
+atoms = read.csv("atoms.csv", stringsAsFactors = FALSE)
+
+aminoAcids = read.csv("amino_acids.csv", stringsAsFactors = FALSE)
+aminoAcids$MonoisotopicMass = sapply(aminoAcids$Composition, function(x){ MonoisotopicMass(formula = ListFormula(x))})
+aminoAcids$MolecularWeight = sapply(aminoAcids$Composition, function(x){ MolecularWeight(formula = ListFormula(x))})
+aminoAcids = data.table(aminoAcids, key = "Abbrev1")
+
+losses = read.csv("losses.csv", stringsAsFactors = FALSE)
+losses[is.na(losses)] = ""
+losses$MonoisotopicMass = sapply(losses$Gain, function(x){ MonoisotopicMass(formula = ListFormula(x))})
+losses$MolecularWeight = sapply(losses$Gain, function(x){ MolecularWeight(formula = ListFormula(x))})
+losses$MonoisotopicMass = losses$MonoisotopicMass - sapply(losses$Loss, function(x){ MonoisotopicMass(formula = ListFormula(x))})
+losses$MolecularWeight = losses$MolecularWeight - sapply(losses$Loss, function(x){ MolecularWeight(formula = ListFormula(x))})
--- server.R
+++ server.R
@@ -1,9 +1,43 @@
 library(shiny)
+library(data.table)
 
 shinyServer(function(input, output) {
+  
+  #Parse sequence string into the mono mass
+  getMass = function(sequenceString){
+    
+    sequence = grep("[A-z]+", sequenceString, value = TRUE)
+    mods = grep("([0-9]+)", sequenceString, value = TRUE)
+    
+    chars = data.table(Abbrev1 = unlist(strsplit(sequence, split = "")))
+    
+    #A[B, on = 'a', bb := i.b] 
+    chars[aminoAcids, on = "Abbrev1", MonoisotopicMass := MonoisotopicMass]
+    
+    #Add up the aa letters
+    monoMass = sum(chars$MonoisotopicMass)
+    #avgMass = sum(chars$MolecularWeight)
+    
+    #Add in the manual mods
+    
+  }
    
-  output$theoreticalIonTable = renderDT(
-    iris, options = list(lengthChange = FALSE)
-  )
+  output$theoreticalIonTable = renderDT({
+    #Parse sequence
+    Ntermbuild = substring(input$sequence, 1, 2:(nchar(input$sequence))-1)
+    Ctermbuild = substring(input$sequence, 2:(nchar(input$sequence)), nchar(input$sequence))
+    
+    input$fragmentTypes
+    
+    #Generate ions
+    
+    #Return datatable
+    all_ions = rbind(data.frame(sequence = Ntermbuild), data.frame(sequence = Ctermbuild))
+    all_ions = all_ions %>% 
+      rowwise() %>% 
+      mutate(mass = getMass(sequence))
+    
+    datatable(all_ions, options = list(lengthChange = FALSE))
+  })
   
 })
