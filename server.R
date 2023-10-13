ea0c7e3b5b995ef747caa6b8e88a4dd0114b8a5e
bnmoore <benjamin.n.moore@gmail.com>
2023-10-12 23:43:13 -0700

    Added stuff

--- server.R
+++ server.R
@@ -1,44 +1,123 @@
 library(shiny)
 library(data.table)
 library(tidyverse)
+library(Spectra)
+library(rhandsontable)
 
 shinyServer(function(input, output) {
   
   #Parse sequence string into the mono mass
-  getMass = function(sequenceString){
+  getMass = function(sequenceString, mono){
     
     sequence = grep("[A-z]+", sequenceString, value = TRUE)
     mods = grep("([0-9]+)", sequenceString, value = TRUE)
     
     chars = data.table(Abbrev1 = unlist(strsplit(sequence, split = "")))
-    
-    #A[B, on = 'a', bb := i.b] 
-    chars[aminoAcids, on = "Abbrev1", MonoisotopicMass := MonoisotopicMass]
+
+    #A[B, on = 'a', bb := i.b]
     
     #Add up the aa letters
-    monoMass = sum(chars$MonoisotopicMass)
-    #avgMass = sum(chars$MolecularWeight)
+    if(mono){
+      chars[aminoAcids, on = "Abbrev1", MonoisotopicMass := MonoisotopicMass]
+      mass = sum(chars$MonoisotopicMass)
+    }
+    else{
+      chars[aminoAcids, on = "Abbrev1", AverageMass := AverageMass]
+      mass = sum(chars$AverageMass)
+    }
     
     #Add in the manual mods
     
+    format(mass, nsmall = 5)
   }
+  
+  output$elemental_comp = renderText({
+    paste("Elemental Composition:", input$sequence)
+  })
    
   output$theoreticalIonTable = renderDT({
     #Parse sequence
     Ntermbuild = substring(input$sequence, 1, 2:(nchar(input$sequence))-1)
     Ctermbuild = substring(input$sequence, 2:(nchar(input$sequence)), nchar(input$sequence))
     
-    input$fragmentTypes
+    fragment_types = input$fragment_types
     
     #Generate ions
+    #all_ions = tibble()
+    #all_ions$sequence = ""
+    #all_ions[atoms$Abbrev1] = 0
     
-    #Return datatable
-    all_ions = rbind(data.frame(sequence = Ntermbuild), data.frame(sequence = Ctermbuild))
+    all_ions = rbind(
+      data.frame(sequence = Ntermbuild, ion_type = "a"),
+      data.frame(sequence = Ntermbuild, ion_type = "b"),
+      data.frame(sequence = Ntermbuild, ion_type = "c"),
+      data.frame(sequence = Ctermbuild, ion_type = "x"),
+      data.frame(sequence = Ctermbuild, ion_type = "y"),
+      data.frame(sequence = Ctermbuild, ion_type = "z")
+    )
+    
     all_ions = all_ions %>% 
+      left_join(ionTypeList, by = "ion_type") %>% 
       rowwise() %>% 
-      mutate(mass = getMass(sequence))
-    
-    datatable(all_ions, options = list(lengthChange = FALSE))
+      mutate(ion_type = paste0(ion_type, str_length(sequence))) %>% 
+      mutate(mass = getMass(sequence, input$monoOrAvg == "Monoisotopic"))
+               
+    #Return DT
+    datatable(select(all_ions, c("ion_type", "mass")), options = list(lengthChange = FALSE))
   })
   
 })
+
+
+
+
+
+
+# #Parse sequence string into the mono mass
+# getMass = function(sequenceString){
+#   
+#   sequence = grep("[A-z]+", sequenceString, value = TRUE)
+#   mods = grep("([0-9]+)", sequenceString, value = TRUE)
+#   
+#   chars = data.table(Abbrev1 = unlist(strsplit(sequence, split = "")))
+#   
+#   #A[B, on = 'a', bb := i.b] 
+#   chars[aminoAcids, on = "Abbrev1", MonoisotopicMass := MonoisotopicMass]
+#   
+#   #Add up the aa letters
+#   monoMass = sum(chars$MonoisotopicMass)
+#   #avgMass = sum(chars$MolecularWeight)
+#   
+#   #Add in the manual mods
+#   
+# }
+
+# output$theoreticalIonTable = renderDT({
+#   #Parse sequence
+#   Ntermbuild = substring(input$sequence, 1, 2:(nchar(input$sequence))-1)
+#   Ctermbuild = substring(input$sequence, 2:(nchar(input$sequence)), nchar(input$sequence))
+#   
+#   fragment_types = input$fragment_types
+#   
+#   #Generate ions
+#   all_ions = rbind(
+#     data.frame(sequence = Ntermbuild, ion_type = "a"),
+#     data.frame(sequence = Ntermbuild, ion_type = "b"), 
+#     data.frame(sequence = Ntermbuild, ion_type = "c"),
+#     data.frame(sequence = Ctermbuild, ion_type = "x"),
+#     data.frame(sequence = Ctermbuild, ion_type = "y"),
+#     data.frame(sequence = Ctermbuild, ion_type = "z")
+#   )
+#   
+#   all_ions = all_ions %>% 
+#     left_join(ionTypeList, by = "ion_type") %>% 
+#     rowwise() %>% 
+#     mutate(ion_type = paste0(ion_type, str_length(sequence))) %>% 
+#     mutate(mass = getMass(sequence) + 
+#              MonoisotopicMass(list("C" = C,"H" = H,"N" = N,"O" = O,"S" = S,"P" = P,"Br" = Br,"Cl" = Cl,"F" = F,"Si" = Si)))
+#   
+#   #Return DT
+#   datatable(select(all_ions, c("ion_type", "mass")), options = list(lengthChange = FALSE))
+# })
+# 
+# })
--- ui.R
+++ ui.R
@@ -1,22 +1,42 @@
 library(shiny)
 library(DT)
-# library(rsconnect)
-# deployApp()
+library(rhandsontable)
 
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Fragmentor"),
   
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput("fragmentTypes", "Ion types:", choiceNames = ionTypeList, choiceValues = ionTypeList
-      )
-    ),
+  fluidRow(
+    column(width = 1, "Nterm"),
+    column(width = 8, textAreaInput("sequence", "Sequence", "RGYALG", width = "80%")),
+    column(width = 1),
+    column(width = 1, "Cterm"),
+    column(width = 1, "Polarity")
+  ),
+  
+  fluidRow(
+    column(width = 4, textOutput("elemental_comp")),
+    column(width = 4, "Deuterium Exchange")
+  ),
+  
+  fluidRow(
+    column(width = 1,
+           radioButtons("monoOrAvg", label = "", choices = c("Monoisotopic", "Average")),
+           checkboxGroupInput("fragment_types", "Ion types:", 
+                              choiceNames = ionTypeList$ion_type, 
+                              choiceValues = ionTypeList$ion_type),
+           "Sidechains",
+           "Losses",
+           "Charge States"
+           ),
     
-    mainPanel(
-      textAreaInput("sequence", "Sequence", "RGYALG", width = "80%"),
-      DTOutput("theoreticalIonTable")
+    column(width = 8,
+           DTOutput("theoreticalIonTable")
+           ),
+    
+    column(width = 3,
+           "Mass Delta area"
     )
   )
 ))
