82e2283741ebab982306ae1f9529c64528d74f83
bnmoore <benjamin.n.moore@gmail.com>
2023-10-26 22:47:59 -0700

    Fixed AA mass errors

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ shinyUI(fluidPage(
   # Application title
   fluidRow(
     column(width = 10, titlePanel("Fragmentor")),
-    column(width = 2, "v2023-10-25")
+    column(width = 2, "v2023-10-26")
   ),
   
   fluidRow(
